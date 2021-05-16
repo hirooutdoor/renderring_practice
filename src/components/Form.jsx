f1095eba2f35e620f4f309895742ffa8bb13217a
hiro <hmaeyama0510@gmail.com>
2021-05-16 12:29:38 +0900

    Second slution This is second solution

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,7 @@ function Form(props) {
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      {props.isRegistered === false && (
+      {props.isRegistered ? null : (
         <input type="password" placeholder="Confirm Password" />
       )}
       <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
